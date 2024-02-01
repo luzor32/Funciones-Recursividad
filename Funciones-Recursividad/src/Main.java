59c34b2656533fa2e1dd7a0790e4a48690dafb3a
lucio albornoz <lucioga32@gmail.com>
2024-02-01 19:49:36 -0300

    ejercicio 4 resuelto

--- Funciones-Recursividad/src/Main.java
+++ Funciones-Recursividad/src/Main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hello world!");
+        System.out.print("Ingresa un numero entre el 2 y el 9: ");
+        int numero = sc.nextInt();
+
+        tablaDeMultiplicar(numero);
+
+    }
+
+    public static void tablaDeMultiplicar(int num){
+        if (num >= 2 && num <=9){
+            System.out.println(" Tabla del "+num);
+
+            for (int i = 1; i <=10 ; i++) {
+                int prod = num * i;
+                System.out.println(num+" x "+i+" = "+prod);
+            }
+        }else {
+            System.out.println("El numero ingresado esta fuera de rango");
+        }
     }
 }
