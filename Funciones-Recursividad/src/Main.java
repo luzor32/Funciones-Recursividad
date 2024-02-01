fc22f40a23efd313e0ac6da3995911bfe4b02455
lucio albornoz <lucioga32@gmail.com>
2024-02-01 01:03:13 -0300

    ejercicio 2 resulto

--- Funciones-Recursividad/src/Main.java
+++ Funciones-Recursividad/src/Main.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa un numero entero: ");
+        int numero = sc.nextInt();
+
+        System.out.println("Números primos desde 1 hasta " + numero + ":");
+        imprimirPrimosHasta(numero);
+
     }
+
+    public static boolean esPrimo(int num, int divisor) {
+        if (divisor <= 1) {
+            return true;
+        }
+        if (num % divisor == 0) {
+            return false;
+        }
+        return esPrimo(num, divisor - 1);
+    }
+
+    public static void imprimirPrimosHasta(int numero) {
+        if (numero >= 1) {
+            imprimirPrimosHasta(numero - 1);
+            if (esPrimo(numero, numero - 1)) {
+                System.out.print(numero + " ");
+            }
+        }
+    }
+
 }
