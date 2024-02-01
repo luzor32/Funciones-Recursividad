961a21c1b55dd9251cb3b155eaf71753ce0becc5
lucio albornoz <lucioga32@gmail.com>
2024-02-01 00:22:37 -0300

    ejecicio 1 resuelto

--- Funciones-Recursividad/src/Main.java
+++ Funciones-Recursividad/src/Main.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa un numero entero: ");
+        int numero = sc.nextInt();
+
+        if (esPrimo(numero,2)){
+            System.out.println("El numero es primo");
+        }else {
+            System.out.println("El numero no es primo");
+        }
+
     }
+
+    public static boolean esPrimo(int nro,int divisor){
+        if (nro == divisor){
+            return true;
+        } else if (nro % divisor ==0 || nro < 2) {
+            return false;
+        }
+        return esPrimo(nro,divisor+1);
+    }
+
 }
